90f466119595e1147f6d9f4df6266e061c31ed4d
samaroo <samaroo@umich.edu>
2021-04-18 13:37:51 -0400

    Fixed redux reducers to accept new API response format

--- src/Redux/ActionCreators/submitText.js
+++ src/Redux/ActionCreators/submitText.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const submitText = (text) => async (dispatch, getState) => {
-    const link = "test";
+    const link = "http://35.194.13.147:8081/api/parse";
     dispatch({type: "SUBMIT"});
     const response = await axios.post(link, {text})
     console.log(response)
--- src/Redux/Reducers/verdictReducer.js
+++ src/Redux/Reducers/verdictReducer.js
@@ -1,7 +1,8 @@
 const verdictReducer = (state = "?", action) => {
     switch (action.type) {
         case 'SUCCESSFUL_API_RESPONSE':
-            return (action.payload.confidence > 0.5 ? "Yes" : "No")
+            //return (action.payload.confidence > 0.5 ? "Yes" : "No")
+            return (action.payload.verdict ? "Yes" : "No")
         case 'UNSUCCESSFUL_API_RESPONSE':
             return "?"
         default:
